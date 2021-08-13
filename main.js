a1ae25c18f3f3374cc75baab38ddc271f377cc92
BlackDragon-B <59388636+BlackDragon-B@users.noreply.github.com>
2021-08-13 18:59:48 +0200

    Changed getCookie function.

--- main.js
+++ main.js
@@ -29,20 +29,10 @@ function getLocation() {
         document.getElementById('info').innerText = "Geolocation is not supported by this browser.";
     });
 }
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
 function setCookie(cname, cvalue) { document.cookie = cname + "=" + cvalue };
 var mapstyle = ''
