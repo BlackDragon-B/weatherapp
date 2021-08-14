a0b3658256739705d3fdd8d3c1e0703f65c9cfee
BlackDragon-B <kyan2007@live.nl>
2021-08-14 20:10:35 +0200

    0.2.3 - Added changing metrics

--- views/main.js
+++ views/main.js
@@ -54,6 +54,7 @@ function getIcon(owmIcon) {
 };
 var lat
 var lon
+var MetricFormula
 // Begin of Map loading and other stuff
 function loadAssets() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYmxhY2tkcmFnb24tYiIsImEiOiJja3NibjEzczkwOGR4Mndwazg4NjloNGx0In0.WrmQ8KgAT95audjoWuAXug'; // Haha, URL restriction go brr.
@@ -69,9 +70,9 @@ function loadAssets() {
         .then(res => res.json())
         .then(data => {
             document.getElementById('wStatus').innerText = data.weather[0].main;
-            document.getElementById('mintemp').innerText = Math.floor(data.main.temp_min - 273);
-            document.getElementById('temp').innerText = Math.floor(data.main.temp - 273);
-            document.getElementById('maxtemp').innerText = Math.floor(data.main.temp_max - 273);
+            document.getElementById('mintemp').innerText = calculateMetric(data.main.temp_min);
+            document.getElementById('temp').innerText = calculateMetric(data.main.temp);
+            document.getElementById('maxtemp').innerText = calculateMetric(data.main.temp_max);
             document.getElementById('humidity').innerText = data.main.humidity;  
             document.getElementById('loc').innerText = `${data.name}, ${data.sys.country}`;
             document.getElementById('windspeed').innerText = data.wind.speed;
@@ -82,21 +83,53 @@ function loadAssets() {
 // End of Map loading and other stuff
 var mapstyle = '';
 var currentmode = 'light';
-function applyTheme() {
-    if (currentmode == 'light') {
+var buttoncolor = '';
+function setTheme(mode) {
+    if (mode == 'dark') {
         mapstyle = 'mapbox://styles/mapbox/dark-v10';
         document.body.style.color = '#cccccc';
+        buttoncolor = '#cccccc';
         document.body.style.backgroundColor = 'White';
         document.getElementById('themeicon').innerHTML = '<h3><span class="iconify" data-icon="mdi-weather-night" style="color: #cccccc"></span></h3>';
-        currentmode = 'dark';
-    } else if (currentmode == 'dark') {
+        document.getElementById('metricicon').innerHTML = '<h3><span class="iconify" data-icon="mdi-thermometer" style="color: #cccccc"></span></h3>';
+    } else if (mode == 'light') {  
         mapstyle = 'mapbox://styles/mapbox/light-v10';
         document.body.style.color = '#333333';
+        buttoncolor = '#333333';
         document.body.style.backgroundColor = 'Black';
         document.getElementById('themeicon').innerHTML = '<h3><span class="iconify" data-icon="mdi-weather-sunny" style="color: #333333"></span></h3>';
-        currentmode = 'light';
+        document.getElementById('metricicon').innerHTML = '<h3><span class="iconify" data-icon="mdi-thermometer" style="color: #333333"></span></h3>';
     };
     loadAssets();
+};
+function applyTheme() {
+    if (currentmode == 'light') {
+        setTheme('dark');
+        currentmode = 'dark';
+    } else if (currentmode == 'dark') {
+        setTheme('light');
+        currentmode = 'light';
+    };
+};
+var currentMetric = 'celsius';
+function toggleMetric() {
+    if (currentMetric == 'celsius') {
+        currentMetric = 'kelvin';
+    } else if (currentMetric == 'kelvin') {
+        currentMetric = 'fahrenheit';
+    } else if (currentMetric == 'fahrenheit') {
+        currentMetric = 'celsius';
+    };   
+    loadAssets();
+};
+function calculateMetric(numb) {
+    if (currentMetric == 'kelvin') {
+        return `${Math.floor(numb).toString()}K`;
+    } else if (currentMetric == 'celsius') {
+        return `${Math.floor((numb-273.15)).toString()}°C`;
+    } else if (currentMetric == 'fahrenheit') {
+        return `${Math.floor(((numb-273.15)*1.8)+32).toString()}°F`;
+    };
 };
 function getLocation() {
     navigator.geolocation.getCurrentPosition((position) => {
